6ae1e5bb4bee6e63611a85b84af31db9b70a4892
Allison Champlin <47014575+AllisonMChamplin@users.noreply.github.com>
2019-07-17 16:33:00 -0500

    Fixed view and controller
    Got this working!

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,1 +1,28 @@
-console.log("HI!");
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function () {
+
+
+    $(".change-eat").on("click", function (event) {
+        var id = $(this).data("id");
+        var newEat = $(this).data("neweat");
+
+        var newEatState = {
+            devoured: newEat
+        };
+
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newEatState
+        }).then(
+            function () {
+                console.log("changed eat to", newEat);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+
+
+});
